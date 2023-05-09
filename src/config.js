e5fbd86527175ec772a1f3d237be20b193b83664
Thomas Stjernegaard Jeppesen <tsjeppesen@gbif.org>
2023-05-09 16:33:17 +0200

    configure to use the reverse proxy for the backend

--- src/config.js
+++ src/config.js
@@ -12,8 +12,8 @@ const prod = {
     taxonSuggestUrl : 'https://api.gbif.org/v1/species/suggest',
     taxonSearchUrl : 'https://api.gbif.org/v1/species/search',
     gbifBackboneKey: 'd7dddbf4-2cf0-4f39-9b2a-bb099caae36c',
-    phylonextWebservice:  'http://phylonext-vh.gbif.org:9000/phylonext', 
-    authWebservice: 'http://phylonext-vh.gbif.org:9000/auth'
+    phylonextWebservice:  'https://phylonext.gbif.org/service/phylonext', 
+    authWebservice: 'https://phylonext.gbif.org/service/auth'
 }
 
 export default prod; //prod;
